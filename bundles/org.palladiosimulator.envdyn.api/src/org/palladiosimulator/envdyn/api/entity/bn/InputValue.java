fd6cb51032ec3a6fc3caceb916b02573f49bf5c4
Ralf Sieger <sieger@fzi.de>
2024-02-09 09:20:58 +0100

    added generic type

--- bundles/org.palladiosimulator.envdyn.api/src/org/palladiosimulator/envdyn/api/entity/bn/InputValue.java
+++ bundles/org.palladiosimulator.envdyn.api/src/org/palladiosimulator/envdyn/api/entity/bn/InputValue.java
@@ -6,30 +6,30 @@ import tools.mdsd.probdist.api.entity.CategoricalValue;
 import tools.mdsd.probdist.api.entity.NumericalValue;
 import tools.mdsd.probdist.api.entity.Value;
 
-public class InputValue {
+public class InputValue<V> {
 
     private final GroundRandomVariable variable;
 
-    private Value<?> value;
+    private Value<V> value;
 
-    private InputValue(Value<?> value, GroundRandomVariable variable) {
+    private InputValue(Value<V> value, GroundRandomVariable variable) {
         this.value = value;
         this.variable = variable;
     }
 
-    public static InputValue create(Value<?> value, GroundRandomVariable variable) {
-        return new InputValue(value, variable);
+    public static <V> InputValue<V> create(Value<V> value, GroundRandomVariable variable) {
+        return new InputValue<>(value, variable);
     }
 
     public GroundRandomVariable getVariable() {
         return variable;
     }
 
-    public Value<?> getValue() {
+    public Value<V> getValue() {
         return value;
     }
 
-    public void setValue(Value<?> value) {
+    public void setValue(Value<V> value) {
         this.value = value;
     }
 
