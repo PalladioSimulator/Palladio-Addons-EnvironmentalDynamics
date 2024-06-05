d68c9fe41e29ce29214f1e6d093998a0c47014e1
Martina Rapp <rapp@fzi.de>
2024-06-05 10:33:26 +0200

    added initial test project to test qvto scripts

---
+++ tests/org.palladiosimulator.envdyn.examples.deltaiot.tests/src/main/java/org/palladiosimulator/envdyn/examples/deltaiot/tests/ChangeLinkPowerTest.java
@@ -0,0 +1,19 @@
+package org.palladiosimulator.envdyn.examples.deltaiot.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChangeLinkPowerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
