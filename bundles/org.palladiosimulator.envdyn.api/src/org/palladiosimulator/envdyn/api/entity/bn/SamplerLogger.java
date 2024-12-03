86bb920c5dc70474fcf2b8313806c90ba887fad8
Ralf Sieger <sieger@fzi.de>
2024-12-03 09:22:44 +0100

    removed temp. SamplerLogger classes

--- bundles/org.palladiosimulator.envdyn.api/src/org/palladiosimulator/envdyn/api/entity/bn/SamplerLogger.java
+++
@@ -1,7 +0,0 @@
-package org.palladiosimulator.envdyn.api.entity.bn;
-
-import org.palladiosimulator.envdyn.environment.staticmodel.GroundRandomVariable;
-
-public interface SamplerLogger {
-    void onSample(String context, GroundRandomVariable variable, Object value);
-}
